a6dc2d082f0dc0bcade45a59eaff2e89afb7ce7a
Valerio <valeriodevivo90@gmail.com>
2021-01-12 17:28:36 +0100

    Finish with two metods

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 $(document).ready(function(){
     $('.main-menu li').hover(function(){
         
-        var indice = $(this).index();
-        console.log(indice);
-
-        if (indice == 3) {
-            $('.drop-account').toggleClass('active');
-        }
-        
-        else if (indice == 4) {
-            $('.drop-prodotti').toggleClass('active');
-        }
-
-        else if (indice == 5) {
-            $('.drop-azienda').toggleClass('active');
-        }
-
-        else if (indice == 6) {
-            $('.drop-aiuto').toggleClass('active');
-        }
-
-        else if (indice == 7) {
-            $('.drop-lingua').toggleClass('active');
-            $('.frecciasu').toggleClass('display');
-            $('.frecciagiu').toggleClass('none');
-
-        }
-        
+        $(this).find('.drop').toggleClass('active');
         
     })
 });
+
+
+
+
+// $(document).ready(function(){
+//     $('.main-menu li').hover(function(){
+        
+//         var indice = $(this).index();
+//         console.log(indice);
+
+//         if (indice == 3) {
+//             $('.drop-account').toggleClass('active');
+//         }
+        
+//         else if (indice == 4) {
+//             $('.drop-prodotti').toggleClass('active');
+//         }
+
+//         else if (indice == 5) {
+//             $('.drop-azienda').toggleClass('active');
+//         }
+
+//         else if (indice == 6) {
+//             $('.drop-aiuto').toggleClass('active');
+//         }
+
+//         else if (indice == 7) {
+//             $('.drop-lingua').toggleClass('active');
+//             $('.frecciasu').toggleClass('display');
+//             $('.frecciagiu').toggleClass('none');
+
+//         }
+        
+
+//     })
+// });
 
